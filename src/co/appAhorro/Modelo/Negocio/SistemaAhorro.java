7b3f7aa14af4330cf1527d0a08983f875c318762
David Molano <david_molano@hotmail.com>
2016-11-29 22:47:47 -0500

    Se comenzo el cuerpo de la clase sistema ahorro. Revisar logica de la aplicacion

--- src/co/appAhorro/Modelo/Negocio/SistemaAhorro.java
+++ src/co/appAhorro/Modelo/Negocio/SistemaAhorro.java
@@ -1,6 +1,44 @@
 package co.appAhorro.Modelo.Negocio;
 
+import java.util.List;
+
+import co.appAhorro.Controlador.Controlador;
+
+/**
+ * @author David Molano
+ *Clase de acceso al sistema, conocida como de Logica o Negocio.
+ *Aqui se difinen las diferentes funcionalidades que ofrece el sistema.
+ *
+ */
+
 public class SistemaAhorro {
+	
+	private Controlador controlador;
+	private List<Ahorro> ahorros;
+	
+	
+	
+	public double registrarAhorro(num) {
+		
+		
+		
+	}
+	
+	private void setDenominacion()
+	
+	
+	
+	
+	
+	public Controlador getControlador() {
+		return controlador;
+	}
+
+	public void setControlador(Controlador controlador) {
+		this.controlador = controlador;
+	}
+	
+	
 	
 	
 
